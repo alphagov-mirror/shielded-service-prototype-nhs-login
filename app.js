06e81195401e948cc5fad11d555300cb46d073d2
Jon Maher <jonmaher@me.com>
2020-06-26 09:42:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var appViews = [
 ]
 
 var nunjucksConfig = {
-  autoescape: true
+    autoescape: true
 }
 
 nunjucksConfig.express = app
@@ -67,39 +67,39 @@ utils.addNunjucksFilters(nunjucksAppEnv)
 // Session uses service name to avoid clashes with other prototypes
 const sessionName = 'nhsuk-prototype-kit-' + (Buffer.from(config.serviceName, 'utf8')).toString('hex')
 let sessionOptions = {
-  secret: sessionName,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 4 // 4 hours
-  }
+    secret: sessionName,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 4 // 4 hours
+    }
 }
 
 // Support session data in cookie or memory
 if (useCookieSessionStore === 'true' && !onlyDocumentation) {
-  app.use(sessionInCookie(Object.assign(sessionOptions, {
-    cookieName: sessionName,
-    proxy: true,
-    requestKey: 'session'
-  })))
+    app.use(sessionInCookie(Object.assign(sessionOptions, {
+        cookieName: sessionName,
+        proxy: true,
+        requestKey: 'session'
+    })))
 } else {
-  app.use(sessionInMemory(Object.assign(sessionOptions, {
-    name: sessionName,
-    resave: false,
-    saveUninitialized: false
-  })))
+    app.use(sessionInMemory(Object.assign(sessionOptions, {
+        name: sessionName,
+        resave: false,
+        saveUninitialized: false
+    })))
 }
 
 
 // Support for parsing data in POSTs
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }))
 
 
 // Automatically store all data users enter
 if (useAutoStoreData === 'true') {
-  app.use(utils.autoStoreData)
-  utils.addCheckedFunction(nunjucksAppEnv)
+    app.use(utils.autoStoreData)
+    utils.addCheckedFunction(nunjucksAppEnv)
 }
 
 
@@ -108,19 +108,19 @@ checkFiles()
 
 
 // Warn if node_modules folder doesn't exist
-function checkFiles () {
-  const nodeModulesExists = fs.existsSync(path.join(__dirname, '/node_modules'))
-  if (!nodeModulesExists) {
-    console.error('ERROR: Node module folder missing. Try running `npm install`')
-    process.exit(0)
-  }
+function checkFiles() {
+    const nodeModulesExists = fs.existsSync(path.join(__dirname, '/node_modules'))
+    if (!nodeModulesExists) {
+        console.error('ERROR: Node module folder missing. Try running `npm install`')
+        process.exit(0)
+    }
 
-  // Create template .env file if it doesn't exist
-  const envExists = fs.existsSync(path.join(__dirname, '/.env'))
-  if (!envExists) {
-    fs.createReadStream(path.join(__dirname, '/lib/template.env'))
-      .pipe(fs.createWriteStream(path.join(__dirname, '/.env')))
-  }
+    // Create template .env file if it doesn't exist
+    const envExists = fs.existsSync(path.join(__dirname, '/.env'))
+    if (!envExists) {
+        fs.createReadStream(path.join(__dirname, '/lib/template.env'))
+            .pipe(fs.createWriteStream(path.join(__dirname, '/.env')))
+    }
 }
 
 // Create template session data defaults file if it doesn't exist
@@ -129,19 +129,19 @@ const sessionDataDefaultsFile = path.join(dataDirectory, '/session-data-defaults
 const sessionDataDefaultsFileExists = fs.existsSync(sessionDataDefaultsFile)
 
 if (!sessionDataDefaultsFileExists) {
-  console.log('Creating session data defaults file')
-  if (!fs.existsSync(dataDirectory)) {
-    fs.mkdirSync(dataDirectory)
-  }
+    console.log('Creating session data defaults file')
+    if (!fs.existsSync(dataDirectory)) {
+        fs.mkdirSync(dataDirectory)
+    }
 
-  fs.createReadStream(path.join(__dirname, '/lib/template.session-data-defaults.js'))
-    .pipe(fs.createWriteStream(sessionDataDefaultsFile))
+    fs.createReadStream(path.join(__dirname, '/lib/template.session-data-defaults.js'))
+        .pipe(fs.createWriteStream(sessionDataDefaultsFile))
 }
 
 // Check if the app is documentation only
-if(onlyDocumentation !== 'true') {
-  // Require authentication if not
-  app.use(authentication);
+if (onlyDocumentation !== 'true') {
+    // Require authentication if not
+    app.use(authentication);
 }
 
 // Local variables
@@ -157,87 +157,87 @@ app.use('/nhsuk-frontend', express.static(path.join(__dirname, 'node_modules/nhs
 app.use('/nhsuk-frontend', express.static(path.join(__dirname, 'node_modules/nhsuk-frontend/dist')));
 
 
-// Check if the app is documentation only
+/* Check if the app is documentation only
 if(onlyDocumentation == 'true') {
   app.get('/', function(req, res) {
     // Redirect to the documentation pages if it is
     res.redirect('/docs');
   });
-}
+} */
 
 // Use custom application routes
 app.use('/', routes);
 
 // Automatically route pages
 app.get(/^([^.]+)$/, function (req, res, next) {
-  automaticRouting.matchRoutes(req, res, next)
+    automaticRouting.matchRoutes(req, res, next)
 })
 
 // Check if the app is using documentation
 if (useDocumentation || onlyDocumentation == 'true') {
-  // Documentation routes
-  app.use('/docs', documentationApp);
+    // Documentation routes
+    app.use('/docs', documentationApp);
 
-  // Nunjucks configuration for documentation
-  var docViews = [
+    // Nunjucks configuration for documentation
+    var docViews = [
     path.join(__dirname, 'docs/views/'),
     path.join(__dirname, 'node_modules/nhsuk-frontend/packages/components')
   ]
 
-  var nunjucksAppEnv = nunjucks.configure(docViews, {
-    autoescape: true,
-    express: documentationApp
-  });
-  nunjucksAppEnv.addGlobal('version', packageInfo.version);
+    var nunjucksAppEnv = nunjucks.configure(docViews, {
+        autoescape: true,
+        express: documentationApp
+    });
+    nunjucksAppEnv.addGlobal('version', packageInfo.version);
 
-  // Add Nunjucks filters
-  utils.addNunjucksFilters(nunjucksAppEnv)
+    // Add Nunjucks filters
+    utils.addNunjucksFilters(nunjucksAppEnv)
 
-  // Automatically store all data users enter
-  if (useAutoStoreData === 'true') {
-    documentationApp.use(utils.autoStoreData)
-    utils.addCheckedFunction(nunjucksAppEnv)
-  }
+    // Automatically store all data users enter
+    if (useAutoStoreData === 'true') {
+        documentationApp.use(utils.autoStoreData)
+        utils.addCheckedFunction(nunjucksAppEnv)
+    }
 
-  // Support for parsing data in POSTs
-  documentationApp.use(bodyParser.json());
-  documentationApp.use(bodyParser.urlencoded({
-    extended: true
-  }))
+    // Support for parsing data in POSTs
+    documentationApp.use(bodyParser.json());
+    documentationApp.use(bodyParser.urlencoded({
+        extended: true
+    }))
 
-  // Custom documentation routes
-  documentationApp.use('/', documentationRoutes);
+    // Custom documentation routes
+    documentationApp.use('/', documentationRoutes);
 
-  // Automatically route documentation pages
-  documentationApp.get(/^([^.]+)$/, function (req, res, next) {
-    automaticRouting.matchRoutes(req, res, next)
-  })
+    // Automatically route documentation pages
+    documentationApp.get(/^([^.]+)$/, function (req, res, next) {
+        automaticRouting.matchRoutes(req, res, next)
+    })
 
 }
 
 // Clear all data in session if you open /examples/passing-data/clear-data
 app.post('/examples/passing-data/clear-data', function (req, res) {
-  req.session.data = {}
-  res.render('examples/passing-data/clear-data-success')
+    req.session.data = {}
+    res.render('examples/passing-data/clear-data-success')
 })
 
 // Redirect all POSTs to GETs - this allows users to use POST for autoStoreData
 app.post(/^\/([^.]+)$/, function (req, res) {
-  res.redirect('/' + req.params[0])
+    res.redirect('/' + req.params[0])
 })
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error(`Page not found: ${req.path}`)
-  err.status = 404
-  next(err)
+    var err = new Error(`Page not found: ${req.path}`)
+    err.status = 404
+    next(err)
 })
 
 // Display error
 app.use(function (err, req, res, next) {
-  console.error(err.message)
-  res.status(err.status || 500)
-  res.send(err.message)
+    console.error(err.message)
+    res.status(err.status || 500)
+    res.send(err.message)
 })
 
 // Run the application
